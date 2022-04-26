ed10709796b3347739b4228e1406781e9681291b
millerr Li <s0950311@gmail.com>
2022-04-27 00:06:35 +0800

    initial commit

--- app/src/main/java/com/millerr/bmi/MainActivity.kt
+++ app/src/main/java/com/millerr/bmi/MainActivity.kt
@@ -2,10 +2,24 @@ package com.millerr.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import com.millerr.bmi.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun bmi(view: View){
+        val height = ed_height.text.toString().toFloat()
+        val weight = ed_weight.text.toString().toFloat()
+        val bmi = weight / (height * height)
+        Log.d("BMI", bmi.toString())
     }
 }
